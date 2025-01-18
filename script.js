36c27ab26e10a5d745791dd13e1c049bfeee4518
Rupala Jainish S. <127884177+KINGs2302@users.noreply.github.com>
2025-01-18 15:35:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,11 @@
         const degrees = (percent / 100) * 360;
         circle.style.background = `conic-gradient(#76ABAE ${degrees}deg, #31363F ${degrees}deg)`;
     });
+    if (navigator.userAgent.match(/Mobile|Android|iPhone|iPad/i)) {
+        document.body.style.zoom = "80%"; // Scale down for mobile browsers
+    }
+if (/Mobi|Android/i.test(navigator.userAgent)) {
+    window.location.href = "desktop-only-message.html";
+}
+
 
